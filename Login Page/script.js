ccb8e436ad8c759851261edcca928acb58fe59c1
TripleHay321 <67393023+TripleHay321@users.noreply.github.com>
2023-03-21 23:30:44 -0700

    Add files via upload

---
+++ Login Page/script.js
@@ -0,0 +1,17 @@
+let a = document.getElementById("body");
+let b = document.getElementById("password");
+let c = document.getElementById("username");
+let d = document.getElementById("login");
+let e = document.getElementById("forgotten");
+let f = document.getElementById("button");
+
+
+function show() {
+    a.style.background = "black";
+    b.style.color = "white";
+    c.style.color = "white";
+    d.style.color = "white";
+    e.style.color = "white";
+    f.style.background = "white";
+}
+
